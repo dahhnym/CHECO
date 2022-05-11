6b1b2d5a89388087cf464680e19e10600969e3ac
Danim Kim <kdn725@gmail.com>
2022-05-11 22:03:14 +0900

    ✨ Coin List page

---
+++ src/routes/Coins.tsx
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  min-height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1.5rem 0;
+`;
+
+const CoinList = styled.ul``;
+
+const Coin = styled.li`
+  font-size: 1rem;
+  background-color: ${(props) => props.theme.textColor};
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 1rem;
+  margin-bottom: 10px;
+  a {
+    display: flex;
+    align-items: center;
+    transition: color 0.1s ease-in-out;
+    padding: 20px;
+  }
+  &:hover {
+    color: ${(props) => props.theme.accentColor};
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  color: ${(props) => props.theme.textColor};
+  font-weight: 700;
+`;
+
+const Loader = styled.div`
+  text-align: center;
+`;
+
+const Img = styled.img`
+  width: 25px;
+  height: 25px;
+  margin-right: 10px;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
+const Coins = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('https://api.coinpaprika.com/v1/coins');
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Title>Coin List</Title>
+      </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinList>
+          {coins.map((coin: CoinInterface) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`} state={{ name: coin.name }}>
+                <Img
+                  src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                  alt="coin icon"
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinList>
+      )}
+    </Container>
+  );
+};
+
+export default Coins;
