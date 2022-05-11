543fcb85f16b3038b0d897db76908884c4c6885f
Danim Kim <kdn725@gmail.com>
2022-05-12 01:51:29 +0900

    💄 update theme color

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,7 @@ import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
   bgColor: '#F8F9FF',
-  textColor: '#0D1B64',
+  textColor: '#222B62',
   accentColor: '#51E3C3',
+  textColorBlack: '100F0D',
 };
