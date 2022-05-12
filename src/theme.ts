5469ce8e8174770925962cf6e0f4913c73a723f6
Danim Kim <kdn725@gmail.com>
2022-05-13 00:36:02 +0900

    💄 update theme color

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,16 @@
 import { DefaultTheme } from 'styled-components';
 
-export const theme: DefaultTheme = {
+export const lightTheme: DefaultTheme = {
   bgColor: '#F8F9FF',
   textColor: '#222B62',
   accentColor: '#51E3C3',
-  textColorBlack: '100F0D',
+  textColorBlack: '#100F0D',
+  hoverColor: '#3c4cac',
+};
+export const darkTheme: DefaultTheme = {
+  bgColor: '#222B62',
+  textColor: '#F8F9FF',
+  accentColor: '#51E3C3',
+  textColorBlack: '#100F0D',
+  hoverColor: '#B4BCDC',
 };
