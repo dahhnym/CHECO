0df9101eee798f81a3a397082acb16265fabca93
Danim Kim <kdn725@gmail.com>
2022-05-12 19:29:02 +0900

    ✨ line chart for price change history

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,88 @@
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
+import { fetchCoinHistory } from '../api';
+import ApexChart from 'react-apexcharts';
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
+const Chart = () => {
+  const { coinId } = useParams();
+  const { isLoading, data } = useQuery<IHistorical[]>(
+    ['ohlcv', coinId],
+    () => fetchCoinHistory(coinId!),
+    {
+      refetchInterval: 5000,
+    },
+  );
+  return (
+    <div>
+      {isLoading ? (
+        'Loading chart...'
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: 'Price',
+              data: data?.map((price) => price.close) as number[],
+            },
+          ]}
+          options={{
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: 'transparent',
+            },
+            stroke: {
+              curve: 'smooth',
+              width: 2,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              categories: data?.map((price) => price.time_close),
+              type: 'datetime',
+              labels: {
+                show: false,
+                datetimeFormatter: {
+                  year: 'yyyy',
+                  month: "mmm 'yy",
+                },
+              },
+              tooltip: {
+                enabled: false,
+              },
+            },
+            fill: {
+              type: 'gradient',
+              gradient: { gradientToColors: ['#0be881'], stops: [0, 100] },
+            },
+            colors: ['#0fbcf9'],
+            tooltip: {
+              y: {
+                formatter: (value) => `$ ${value.toFixed(3)}`,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Chart;
