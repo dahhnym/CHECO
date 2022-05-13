147916aa60a82bcf790088638e1075ab84bec6fc
Danim Kim <kdn725@gmail.com>
2022-05-13 23:14:28 +0900

    💄 update theme colors

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -4,8 +4,10 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     textColor: string;
     bgColor: string;
+    boxBgColor: string;
     accentColor: string;
     textColorBlack: string;
     hoverColor: string;
+    boxShadow: string;
   }
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,16 +1,20 @@
 import { DefaultTheme } from 'styled-components';
 
 export const lightTheme: DefaultTheme = {
-  bgColor: '#F8F9FF',
-  textColor: '#222B62',
+  bgColor: '#F7F8FD',
+  boxBgColor: '#fff',
+  textColor: '#160042',
   accentColor: '#51E3C3',
   textColorBlack: '#100F0D',
   hoverColor: '#3c4cac',
+  boxShadow: '#eeeff7',
 };
 export const darkTheme: DefaultTheme = {
-  bgColor: '#222B62',
+  bgColor: '#0F1520',
+  boxBgColor: '#1D242E',
   textColor: '#F8F9FF',
   accentColor: '#51E3C3',
   textColorBlack: '#100F0D',
   hoverColor: '#B4BCDC',
+  boxShadow: 'none',
 };
